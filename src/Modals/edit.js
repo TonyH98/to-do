9aae72a235cc6a26cd98e5ebcd8cc6794ec19207
TonyH98 <tonyhoang@pursuit.org>
2023-12-11 13:30:40 -0500

    Edit form works

--- src/Modals/edit.js
+++ src/Modals/edit.js
@@ -5,7 +5,37 @@ import {useState , useEffect} from "react"
 
 export default function edit({onClose , open, todo}){
 
+let [edit , setEdit] = useState({
+    title: "",
+    details: ""
+})
 
+useEffect(() => {
+    if(todo){
+        setEdit(todo)
+    }
+}, [todo])
+
+function handleText(e){
+    const {name , value} = e.target
+    setEdit((prev) => ({
+        ...prev,
+        [name]: value
+    }))
+}
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+        await changeDetails(todo.id, edit.title, edit.details)
+        onClose()
+    }
+    catch(error){
+        console.log(error)
+    }
+}
     if(!open) return null
 
 
@@ -17,7 +47,23 @@ export default function edit({onClose , open, todo}){
           </div>
           <div className="content">
           <h2>Change Todo Items</h2>
-           
+           <form onSubmit={handleSubmit}>
+           <input
+            value={edit.title}
+            name="title"
+            type="text"
+            onChange={handleText}
+            />
+            <input
+            value={edit.details}
+            name="details"
+            type="text"
+            onChange={handleText}
+            />
+
+            <button type="submit">Submit</button>
+            <button onClick={onClose}>Cancel</button>
+           </form>
           </div>
 
         </div>
