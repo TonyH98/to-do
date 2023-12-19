e70170e1270d8cc0da6af71d0bb0d15244b39c9e
TonyH98 <tonyhoang@pursuit.org>
2023-12-19 14:27:54 -0500

    Finished the To Do App

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -9,7 +9,11 @@ export default function ToDoItem({ changeDetails, todo, toggleToDo, deleteToDo }
 
 let [modal , setModal] = useState(false)
 
+let [hidden , setHidden] = useState(false)
 
+function handleHidden(){
+  setHidden(!hidden)
+}
 
   return (
     <div>
@@ -21,13 +25,23 @@ let [modal , setModal] = useState(false)
         onChange={(e) => toggleToDo(todo.id, e.target.checked)}
       />
 
-      <Link href={`/items/${todo.id}`}>
-     
-          <h1>{todo.title}</h1>
-    
-      </Link>
 
+      <div >
+      <Link href={`/items/${todo.id}`}>
+      <h1 className={` ${todo.complete ? 'line-through' : ''}`}>{todo.title}</h1>
+      </Link>
       </div>
+
+
+      <button className="ml-3" onClick={handleHidden}>Show Details</button>
+      </div>
+
+      {hidden ? (
+        <div className="mt-3 mb-3 flex flex-col gap-y-3">
+        <p>Details: {todo.details}</p>
+        <span>Status: {!todo.complete ? "Not Complete" : "Complete"}</span>
+        </div>
+      ) : null}
 
       <div className="flex flex-row gap-x-4 mt-3">
 
